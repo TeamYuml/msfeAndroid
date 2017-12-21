e98ca2f8547e063dc64e424836fc939d72f2311e
kaniak274 <kaniak274@gmail.com>
2017-12-21 22:43:36 +0100

    Change and fix layout for timetable

--- Start_APP/app/src/main/java/com/example/konrad/start_app/Harmonogram.java
+++ Start_APP/app/src/main/java/com/example/konrad/start_app/Harmonogram.java
@@ -1,14 +1,7 @@
 package com.example.konrad.start_app;
 
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -46,48 +39,17 @@ public class Harmonogram extends AppCompatActivity {
         TextView textView = (TextView)findViewById(R.id.name);
         textView.setText("Harmonogram na dzien: " + DateProperty.getInstance().date);
 
-        LinearLayout ll = (LinearLayout)findViewById(R.id.ll2);
+        LinearLayout ll = (LinearLayout)findViewById(R.id.ll4);
 
         for (int i = 0; i < obj.length; i++) {
             TextView tv = new TextView(this);
             tv.setTextSize(20f);
-            tv.setPadding(0,0,0, 5);
+            tv.setPadding(0, 0, 0, 5);
             tv.setText("Godzina podania: " + daneDoHarmonogramu[i][0] + "\n"
                     + "Nazwa leku: " + daneDoHarmonogramu[i][1] + "\n");
             tv.setId(i);
             ll.addView(tv);
         }
-
-        /*Button calDFC = (Button) findViewById(R.id.button2);
-        Button sos = (Button) findViewById(R.id.sos);
-        calDFC.setOnClickListener(new View.OnClickListener() {
-            @Override
-            //Metoda która uruchamia na telefonie numer w postaci stringa ACTION_CALL automatycznie
-            public void onClick(View v) {
-                Intent callintent = new Intent(Intent.ACTION_CALL);
-                //z ustawien String
-                callintent.setData(Uri.parse("2238666658"));
-                if (ActivityCompat.checkSelfPermission(Harmonogram.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-
-                    return;
-                }
-                startActivity(callintent);
-            }
-
-        });
-        sos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent sos = new Intent(Intent.ACTION_CALL);
-                //z ustawien String
-                sos.setData(Uri.parse("2238666658"));
-                if (ActivityCompat.checkSelfPermission(Harmonogram.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-
-                    return;
-                }
-                startActivity(sos);
-            }
-        }); */
     }
 }
 
