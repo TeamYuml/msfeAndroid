664ec4450c36bc2de5f04d31e50bc25931ec272d
kaniak274 <kaniak274@gmail.com>
2018-01-18 12:48:47 +0100

    Organize imports

--- Start_APP/app/src/main/java/com/example/konrad/start_app/For_Login.java
+++ Start_APP/app/src/main/java/com/example/konrad/start_app/For_Login.java
@@ -1,10 +1,8 @@
 package com.example.konrad.start_app;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class For_Login extends AppCompatActivity {
 
